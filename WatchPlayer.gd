ef45b9715e4bc24c07c24513e8afc360f00020b1
aturtledev <ashellofaturtle@gmail.com>
2026-05-21 20:36:09 -0700

    even more scuffed now

--- WatchPlayer.gd
+++ WatchPlayer.gd
@@ -3,8 +3,25 @@ extends Node
 @export var body: CharacterBody3D
 @export var camera: Camera3D
 
+var new_pos: Vector3
+
+func _ready():
+	new_pos = body.position + -5*-body.transform.basis.z
+
 func _physics_process(delta):
 	camera.look_at(body.position)
 	
-	if camera.position.distance_to(body.position) > 3:
-		camera.position += camera.position.direction_to(body.position)*.1
+	var dir:Vector3 = camera.position.direction_to(body.position)
+	
+	dir = dir.normalized()
+	
+	dir = dir.clamp(Vector3(-1,0,-1),Vector3(1,0,1))
+	
+	if camera.position.lerp(new_pos,.7).distance_to(body.position) > 2.5:
+		new_pos += dir*.1
+	elif camera.position.lerp(new_pos,.7).distance_to(body.position) < 2:
+		new_pos -= dir*.1
+	
+	new_pos.y = body.position.y + .5
+	
+	camera.position = camera.position.lerp(new_pos, .02)
